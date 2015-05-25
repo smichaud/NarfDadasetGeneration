fdfb9af72e69fd80f20d04a9f514d216f29078f9
smichaud <sebastien.michaud.2@gmail.com>
2015-05-25 10:13:36 -0400

    extracted some functions

--- narf_place_recognition/src/bag_to_dataset.cpp
+++ narf_place_recognition/src/bag_to_dataset.cpp
@@ -7,15 +7,18 @@
 #include "pointmatcher/PointMatcher.h"
 #include "pointmatcher_ros/point_cloud.h"
 
-// [TODO]: Save it with num suffix - 2015-05-21 12:36pm
+#include <iostream>
+#include <iomanip>
+
 // [TODO]: Do ICP for next (with odom?) - 2015-05-21 12:36pm
-// [TODO]: Save as PCD - 2015-05-21 12:36pm
 // [TODO]: Try to get the far range pointcloud - 2015-05-21 12:37pm
-// [TODO]: scan_001.pcd with scan_001_far.pcd - 2015-05-21 12:38pm
+// If your scan_001_far_ranges.pcd.
 // [TODO]: scan_001_info.dat - 2015-05-21 12:38pm
 
 typedef PointMatcher<float> PM;
-using namespace std;
+
+bool managePointCloudMsg(rosbag::MessageInstance const &msg, int index);
+std::string appendNum(std::string input, int numSuffix);
 
 int main(int argc, char **argv) {
     rosbag::Bag bag;
@@ -27,23 +30,45 @@ int main(int argc, char **argv) {
 
     rosbag::View view(bag, rosbag::TopicQuery(topics));
 
-    int sample_index = 0;
+    int pointcloudIndex = 0;
     BOOST_FOREACH(rosbag::MessageInstance const msg, view) {
         std::cout << "Msg received :  " << msg.getDataType() << std::endl;
-        boost::shared_ptr<sensor_msgs::PointCloud2> cloud_msg =
-            msg.instantiate<sensor_msgs::PointCloud2>();
 
-        if(cloud_msg != NULL) {
-            PM::DataPoints pmCloud(
-                    PointMatcher_ros::rosMsgToPointMatcherCloud<float>(
-                        *cloud_msg));
-            int nbPoints = pmCloud.features.cols();
-            ROS_INFO("PointCloud received with %d points", nbPoints);
-
-            pmCloud.save("/home/smichaud/Desktop/test/test.pcd");
+        if(managePointCloudMsg(msg, pointcloudIndex)) {
+            pointcloudIndex++;
         }
     }
 
     bag.close();
 }
 
+bool managePointCloudMsg(rosbag::MessageInstance const &msg, int index) {
+    boost::shared_ptr<sensor_msgs::PointCloud2> cloud_msg =
+        msg.instantiate<sensor_msgs::PointCloud2>();
+
+    if(cloud_msg != NULL) {
+        PM::DataPoints pmCloud(
+                PointMatcher_ros::rosMsgToPointMatcherCloud<float>(
+                    *cloud_msg));
+        int nbPoints = pmCloud.features.cols();
+        ROS_INFO("PointCloud received with %d points", nbPoints);
+
+        std::string path = "/home/smichaud/Desktop/test/";
+        std::string filename = "scan_";
+        filename = appendNum(filename, index);
+        filename += ".pcd";
+
+        pmCloud.save(path + filename);
+
+        return true;
+    }
+    return false;
+}
+
+std::string appendNum(std::string input, int numSuffix) {
+    std::ostringstream output;
+
+    output << input << std::setfill('0') << std::setw(3) << numSuffix;
+
+    return output.str();
+}
